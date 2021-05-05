f18f63c8b4c8f2f46f5a9e2079d32ef83a9e7534
Tajinder Singh Dhoot <singhtazinder047@gmail.com>
2021-05-05 11:46:12 -0400

    changes in base file (starts browser)

--- 3_sample_project/src/org/web/automation/base/InstanciateDriver.java
+++ 3_sample_project/src/org/web/automation/base/InstanciateDriver.java
@@ -1,24 +1,34 @@
 package org.web.automation.base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.web.automation.pages.Login;
+import org.web.automation.pages.Registration;
+import org.web.automation.utility.PropertyReader;
 
 public class InstanciateDriver {
 	
-	WebDriver driver = null;
+	public WebDriver driver = null;
+	public Registration register = null;
+	public Login login = null;
 	
 	@BeforeMethod
 	public void startBrowser() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("https://thetestingworld.com/");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//a[text()='Login']")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//a[text()='Registartion ']")).click();
+		driver.get(PropertyReader.readConfigProperty("application_url"));
 		
+		register = new Registration(driver);
+		login = new Login(driver);
+
 	}
+	
+	/*@AfterMethod
+	public void closeBrowser() throws Exception {
+		Thread.sleep(3000);
+		driver.quit();
+	}*/
 
 }
